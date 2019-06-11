234dc0d87d97f491961ba7a15e47616f379295ae
Sean Gowing <smgowing@hotmail.com>
2019-06-10 17:11:49 -0700

    Updates

---
+++ precache-manifest.285fd17cbc69a3fd68b2ce6b3c0ed2d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2e3dfbda2cd9a9b332ca545c0552b0",
+    "url": "/github-finder-vanilla/index.html"
+  },
+  {
+    "revision": "e94f34105412deaf5272",
+    "url": "/github-finder-vanilla/static/css/main.b4f77495.chunk.css"
+  },
+  {
+    "revision": "19a136f20acaf771a511",
+    "url": "/github-finder-vanilla/static/js/2.4847decd.chunk.js"
+  },
+  {
+    "revision": "e94f34105412deaf5272",
+    "url": "/github-finder-vanilla/static/js/main.c6b0e7ce.chunk.js"
+  },
+  {
+    "revision": "d56c7f32cb5ede393a51",
+    "url": "/github-finder-vanilla/static/js/runtime~main.bcbd2a28.js"
+  }
+]);
